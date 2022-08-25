b653412d1e5ea2934bcb9c8700627f61a5e77e61
Tamzeed Quazi <74509993+tamzeedq@users.noreply.github.com>
2022-08-25 04:13:41 +0000

    Implemented Polls and Coin Flips

--- main.py
+++ main.py
@@ -1,24 +1,112 @@
 import os
 import discord
 import random
+import asyncio
 
-client = discord.Client()
+#import music
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = "$")
+
+# cogs = [music]
+
+# for i in range(len(cogs)):
+#   cogs[i].setup(client)
+
 
 @client.event
 async def on_ready():
   print(f"{client.user} logged in")
+  client.load_extension("dch")
 
-@client.event
-async def on_message(message):
-  if message.content.startswith("$test"):
-    await message.channel.send("test response")
+# @client.event
+# async def on_message(message):
+#   if message.content.startswith("chauffeur"):
+#     await message.channel.send("bruh not this again")
 
-  if message.content.startswith("$flip"):
-    randomInt = random.randint(0, 1)
-    if(randomInt == 0):
-      await message.channel.send("Heads")
-    elif(randomInt == 1):
-      await message.channel.send("Tails")
-      
+@client.command()
+async def coin(ctx):
+  randomInt = random.randint(0, 1)
+  randomGif = random.randint(1, 7)
+  
+  await ctx.send(file = discord.File('coinFlip' + str(randomGif) + '.gif'), delete_after = 5)
+  await asyncio.sleep(5)
+  
+  if(randomInt == 0):
+    await ctx.send("Heads")
+  elif(randomInt == 1):
+    await ctx.send("Tails")
+
+@client.command()
+async def randNum(ctx, min, max):
+  randomInt = random.randint(int(min), int(max))
+  
+  await ctx.send(randomInt)
+  
+
+@client.command()
+async def testEmbed(ctx):
+  embed = discord.Embed(
+    title = 'Title',
+    description = 'Test description',
+    colour = discord.Colour.red()
+  )
+
+  author = ctx.message.author
+  userName = author.name
+  
+  embed.set_footer(text = 'Footer')
+  embed.set_image(url = 'https://wallpaperaccess.com/full/1445568.jpg')
+  embed.set_thumbnail(url = 'https://images.alphacoders.com/927/927310.jpg')
+  embed.set_author(name = userName, icon_url = author.avatar_url)
+  embed.add_field(name = 'Field Name', value = 'Field Value', inline = False)
+  embed.add_field(name = 'Field Name', value = 'Field Value', inline = False)
+  embed.add_field(name = 'Field Name', value = 'Field Value', inline = False)
+
+  await ctx.send(embed = embed)
+
+@client.command()
+async def poll(ctx, *, options):
+  optionList = options.split(" ")
+  emoji = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣"]
+  
+  embed = discord.Embed(
+    title = 'Vote',
+    description = 'React to the message to vote',
+    colour = discord.Colour.red()
+  )
+
+  author = ctx.message.author
+  userName = author.name
+  embed.set_thumbnail(url = 'https://images.alphacoders.com/927/927310.jpg')
+  embed.set_author(name = userName, icon_url = author.avatar_url)
+
+  for x in range(len(optionList)):
+    embed.add_field(name = 'Option ' + emoji[x], value = optionList[x], inline = False)
+    
+  message = await ctx.send(embed = embed)
+
+  for x in range(len(optionList)):
+    await message.add_reaction(emoji[x])
+
+  await ctx.message.delete()
+
+@client.command()
+async def hoppon(ctx):
+  embed = discord.Embed(
+    title = 'Commands',
+    description = 'List of commands',
+    colour = discord.Colour.red()
+  )
+  
+  embed.set_footer(text = 'Footer')
+  embed.set_image(url = 'https://wallpaperaccess.com/full/1445568.jpg')
+  embed.set_thumbnail(url = 'https://images.alphacoders.com/927/927310.jpg')
+  embed.set_author(name = client.user.name, icon_url = client.user.avatar_url)
+  embed.add_field(name = 'Field Name', value = 'Field Value', inline = False)
+  embed.add_field(name = 'Field Name', value = 'Field Value', inline = False)
+  embed.add_field(name = 'Field Name', value = 'Field Value', inline = False)
+
+  await ctx.send(embed = embed)
 my_secret = os.environ['TOKEN']
 client.run(my_secret)
