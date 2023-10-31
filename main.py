a8a93bb0898109fffc4dc0552a53531a87ffd8b1
tamzeedq <74509993+tamzeedq@users.noreply.github.com>
2023-10-31 19:11:44 +0000

    Added command to get prayer times

--- main.py
+++ main.py
@@ -22,12 +22,6 @@ async def on_ready():
     client.load_extension("dch")
 
 
-# @client.event
-# async def on_message(message):
-#   if message.content.startswith("chauffeur"):
-#     await message.channel.send("bruh not this again")
-
-
 @client.command()
 async def coin(ctx):
     randomInt = random.randint(0, 1)
